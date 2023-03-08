9f62fe19ee6f628c84d36ea66dbde85066ecaccd
Avik Pal <avikpal@mit.edu>
2023-03-08 18:45:27 -0500

    try fixing the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,12 +10,21 @@ using Test, TruncatedStacktraces
         showerror(io, e)
         error_msg[] = String(take!(io))
     end
-    @test error_msg[] ==
-          "UndefVarError: `x` not defined" *
-          "\n\nSome of the types have been truncated in the" *
-          " stacktrace for improved reading. To emit complete " *
-          "information\nin the stack trace, evaluate " *
-          "`TruncatedStacktraces.VERBOSE[] = true` and re-run the code.\n"
+    @static if VERSION >= v"1.9.0-rc1"
+        actual_error_msg = "UndefVarError: `x` not defined" *
+                           "\n\nSome of the types have been truncated in the" *
+                           " stacktrace for improved reading. To emit complete " *
+                           "information\nin the stack trace, evaluate " *
+                           "`TruncatedStacktraces.VERBOSE[] = true` and re-run the code.\n"
+    else
+        actual_error_msg = "UndefVarError: x not defined" *
+                           "\n\nSome of the types have been truncated in the" *
+                           " stacktrace for improved reading. To emit complete " *
+                           "information\nin the stack trace, evaluate " *
+                           "`TruncatedStacktraces.VERBOSE[] = true` and re-run the code.\n"
+    end
+    @test error_msg[] == actual_error_msg
+
     TruncatedStacktraces.VERBOSE[] = true
     try
         x
@@ -24,6 +33,10 @@ using Test, TruncatedStacktraces
         showerror(io, e)
         error_msg[] = String(take!(io))
     end
-    @test error_msg[] ==
-          "UndefVarError: `x` not defined"
+    @static if VERSION >= v"1.9.0-rc1"
+        actual_error_msg = "UndefVarError: `x` not defined"
+    else
+        actual_error_msg = "UndefVarError: x not defined"
+    end
+    @test error_msg[] == actual_error_msg
 end
